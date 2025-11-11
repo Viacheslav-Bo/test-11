3774d61f6ce5620272eb4506e25b7355b51b20e3
Viacheslav-Bo <zotikivna@gmail.com>
2025-11-11 20:47:09 +0200

    fff

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,6 +4,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+import sprite from '/public/icons.svg';
+
 const loader = document.querySelector('.loader');
 const gallery = document.querySelector('.gallery');
 
@@ -33,16 +35,16 @@ export function createGallery(images) {
             <div class="desc-container">
               <ul class="desc-list">
               <li class="icons"><svg class="icon" width="24" height="24">
-            <use href="/icons.svg#icon-like"></use>
+            <use href="${sprite}#icon-like"></use>
           </svg><span>${likes}</span></li>
               <li class="icons"><svg class="icon" width="24" height="24">
-            <use href="/icons.svg#icon-views"></use>
+            <use href="${sprite}#icon-views"></use>
           </svg><span>${views}</span></li>
               <li class="icons"><svg class="icon" width="24" height="24">
-            <use href="/icons.svg#icon-comments"></use>
+            <use href="${sprite}#icon-comments"></use>
           </svg><span>${comments}</span></li>
               <li class="icons"><svg class="icon" width="24" height="24">
-            <use href="/icons.svg#icon-downloads"></use>
+            <use href="${sprite}#icon-downloads"></use>
           </svg><span>${downloads}</span></li>
               </ul>
             </div>
